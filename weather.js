d7d83082c090ba6588e111d9781ed1e032f4c0e8
BoikoVD <boiko.volodymyr.dm@gmail.com>
2021-12-22 01:57:15 +0200

    created api service

--- weather.js
+++ weather.js
@@ -1,14 +1,34 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+	if (!token.length) {
+		printError('Не передан токен');
+		return;
+	}
 	try {
-		await saveKeyValue('token', token);
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess('Токен сохранен');
 	} catch (e) {
 		printError(e.message);
+	}
+};
+
+const getForcast = async () => {
+	try {
+		const weather = await getWeather('moscow');
+		console.log(weather);
+	} catch (e) {
+		if (e?.response?.status == 404) {
+			printError('Неверно указан город');
+		} else if (e?.response?.status == 401) {
+			printError('Неверно указан токен');
+		} else {
+			printError(e.message);
+		}
 	}
 };
 
@@ -23,6 +43,7 @@ const initCLI = () => {
 	if (args.t) {
 		return saveToken(args.t);
 	}
+	getForcast();
 };
 
 initCLI();
