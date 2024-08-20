8b80e92d99ec0530d63cd0d3ae2f37177f7bdcce
BoikoVD <boiko.volodymyr.dm@gmail.com>
2024-08-20 02:28:16 +0200

    update get weather feat., update print text

--- services/api.service.js
+++ services/api.service.js
@@ -1,43 +1,63 @@
-import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
-import axios from 'axios';
+import { getKeyValue, TOKEN_DICTIONARY } from "./storage.service.js";
+import axios from "axios";
 
 const getIcon = (icon) => {
-	switch (icon.slice(0, -1)) {
-		case '01':
-			return '☀️';
-		case '02':
-			return '🌤️';
-		case '03':
-			return '☁️';
-		case '04':
-			return '☁️';
-		case '09':
-			return '🌧️';
-		case '10':
-			return '🌦️';
-		case '11':
-			return '🌩️';
-		case '13':
-			return '❄️';
-		case '50':
-			return '🌫️';
-	}
+    switch (icon.slice(0, -1)) {
+        case "01":
+            return "☀️";
+        case "02":
+            return "🌤️";
+        case "03":
+            return "☁️";
+        case "04":
+            return "☁️";
+        case "09":
+            return "🌧️";
+        case "10":
+            return "🌦️";
+        case "11":
+            return "🌩️";
+        case "13":
+            return "❄️";
+        case "50":
+            return "🌫️";
+    }
 };
 
 const getWeather = async (city) => {
-	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
-	if (!token) {
-		throw new Error('Не задан ключ API');
-	};
-	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
-		params: {
-			q: city,
-			appid: token,
-			lang: 'ru',
-			units: 'metric'
-		}
-	});
-	return data;
+    const token = await getKeyValue(TOKEN_DICTIONARY.token);
+    if (!token) {
+        throw new Error("No API key set");
+    }
+
+    const coord = await getCoord(city, token);
+    const weatherData = await getWeatherData(coord, token);
+
+    return weatherData;
+};
+
+const getCoord = async (city, token) => {
+    const { data } = await axios.get("http://api.openweathermap.org/geo/1.0/direct", {
+        params: {
+            q: city,
+            appid: token,
+        },
+    });
+
+    return { lat: data[0]["lat"], lon: data[0]["lon"] };
+};
+
+const getWeatherData = async (coord, token) => {
+    const { data } = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+        params: {
+            lat: coord.lat,
+            lon: coord.lon,
+            appid: token,
+            lang: "en",
+            units: "metric",
+        },
+    });
+    return data;
 };
 
 export { getWeather, getIcon };
--- services/log.service.js
+++ services/log.service.js
@@ -1,36 +1,38 @@
-import chalk from 'chalk';
-import dedent from 'dedent-js';
+import chalk from "chalk";
+import dedent from "dedent-js";
 
 const printError = (error) => {
-	console.log(chalk.bgRed(' ERROR ') + ' ' + error);
+    console.log(chalk.bgRed(" ERROR ") + " " + error);
 };
 
 const printSuccess = (message) => {
-	console.log(chalk.bgGreen(' SUCCESS ') + ' ' + message);
+    console.log(chalk.bgGreen(" SUCCESS ") + " " + message);
 };
 
 const printHelp = () => {
-	console.log(
-		dedent`
-		${chalk.bgCyan(' HELP ')}
-		Без параметров - вывод погоды
-		-s [CITY] для установки города
-		-h для вывода помощи
-		-t [API_KEY] для сохранения токена
+    console.log(
+        dedent`
+		${chalk.bgCyan(" HELP ")}
+		Without parameters  - get weather information
+		-s [CITY]           - change (save) city
+		-t [API_KEY]        - change (save) token
+		-h                  - show help
 		`
-	);
+    );
 };
 
 const printWeather = (res, icon) => {
-	console.log(
-		dedent`
-		${chalk.bgBlue(' WEATHER ')} Погода в городе ${res.name}
-		${icon}  ${res.weather[0].description}
-		Температура ${res.main.temp} (ощущается как ${res.main.feels_like})
-		Влажность ${res.main.humidity}%
-		Скорость ветра ${res.wind.speed}
+    console.log(
+        dedent`
+			${chalk.bgBlue(" WEATHER ")} Weather in ${res.name}
+
+			${icon}  ${res.weather[0].description}
+			
+			Temperature ${res.main.temp}°С (feels like ${res.main.feels_like}°С)
+			Humidity    ${res.main.humidity}%
+			Wind speed  ${res.wind.speed}m/s
 		`
-	);
-}
+    );
+};
 
 export { printError, printSuccess, printHelp, printWeather };
